4e6f8acf1b782d29f88ef66f677707eba2c41001
CalabashSquash <my_name_uhh@protonmail.com>
2024-05-03 22:34:56 +1000

    Update pottery.sol majorly

--- src/Pottery.sol
+++ src/Pottery.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 // import "openzeppelin/contracts/"
 import {IPYieldTokenV2} from "pendle-v2/contracts/interfaces/IPYieldTokenV2.sol";
+import {MiniHelpers} from "pendle-v2/contracts/libraries/MiniHelpers.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {Ticket} from "./Ticket.sol";
 
@@ -15,17 +16,39 @@ contract Kiln {
     uint256 public ticketIdCounter;
     bytes32 public winningNumber;
 
+    uint256 public lotteryEnd;
+    uint256 public mintWindowEnd;
+
     error NotVRF(address caller);
     error WinningNumberNotSet();
+    error LotteryNotOver();
+    error LotteryEndsAfterYtExpiration();
+    error DepositAfterMintWindow();
 
-    constructor(address _yt, uint256 _roundId, address _vrf) {
+    constructor(address _yt, uint256 _roundId, address _vrf, uint256 _lotteryEnd, uint256 _mintWindowEnd) {
         yt = IPYieldTokenV2(_yt);
         string memory roundId = Strings.toString(_roundId);
         ticket = new Ticket(string.concat("Pottery Round ", roundId), string.concat("PR", roundId));
         vrf = _vrf;
+
+        if (lotteryEnd > yt.expiry()) {
+            revert LotteryEndsAfterYtExpiration();
+        }
+        lotteryEnd = _lotteryEnd;
+
+        if (mintWindowEnd > lotteryEnd) {
+            revert LotteryEndsAfterYtExpiration();
+        }
+        mintWindowEnd _mintWindowEnd;
+
+        // expiry timing logic
+        // DRand, ranDAO
     }
 
-    function depositYT(uint256 buyAmount) public {
+    function depositYT(uint256 buyAmount) external {
+        if (block.timestamp > mintWindowEnd) {
+            revert DepositAfterMintWindow();
+        }
         // Cache storage
         _ticketIdCounter = ticketIdCounter;
 
@@ -38,15 +61,25 @@ contract Kiln {
     }
 
     function vrfCallback(bytes32 randomNumber) public onlyVrf {
+        if (!MiniHelpers.isCurrentlyExpired(lotteryEnd)) {
+            revert LotteryNotOver();
+        }
+
         winningNumber = randomNumber;
     }
 
-    function calculateWinner() public view {
+    function payOutWinner() external {
+        address winner = calculateWinner();
+        // get reward token first
+        yt.
+    }
+
+    function calculateWinner() public view returns (address) {
         if (winningNumber == 0) {
             revert WinningNumberNotSet();
         }
         uint256 winningTicket = winningNumber % ticketIdCounter;
-        address winner = ticket.ownerOf(winningTicket);
+        return ticket.ownerOf(winningTicket);
     }
 
     modifier onlyVrf() {
