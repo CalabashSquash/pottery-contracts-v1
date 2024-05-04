cc40bae8d51e3427a4eab1b16a5f756d2302ed51
coppers-j <12965801+coppers-j@users.noreply.github.com>
2024-05-04 16:51:26 +1000

    filled out config & fixed keeper keystore loading

--- keeper/keeper.py
+++ keeper/keeper.py
@@ -9,13 +9,19 @@ import getpass
 import os
 import json
 import argparse
+import time
+import datetime
+
+def open_json(file):
+    with open(file, 'r') as f:
+        return json.load(f)
 
 
 class Keeper:
     def __init__(self, config: Dict, account: LocalAccount):
-        self.web3 = Web3(Web3.HTTPProvider(config['rpc_url']))
+        self.web3 = Web3(Web3.HTTPProvider(config["rpc_url"]))
         # get enviorment variable PRIVATE_KEY
-        self.account = self.web3.eth.account.privateKeyToAccount(private_key)
+        self.account = account
 
         if config["vrf"]:
             abi = "vrf_abi.json"
@@ -23,35 +29,38 @@ class Keeper:
             abi = "abi.json"
 
         self.contract = self.web3.eth.contract(
-            address=config['contract_address'],
-            abi=abi
+            address=config["contract_address"], abi=abi
         )
-        self.deadline = config['deadline']
+        self.deadline = config["deadline"]
 
+        print(f"Deadline is {datetime.datetime.fromtimestamp(self.deadline)}")
+        print(f"Time now is {datetime.datetime.now()}")
 
         self.w3 = None
-        self.w3.middleware_onion.add(construct_sign_and_send_raw_middleware(self.account))
+        self.w3.middleware_onion.add(
+            construct_sign_and_send_raw_middleware(self.account)
+        )
 
         print(f"Your hot wallet address is {self.account.address}")
         self.balance = self.w3.eth.get_balance(self.account.address)
         print(
-            f"Your hot wallet balance is {OneInch.parse_float(self.w3.from_wei(self.balance, 'ether'))} {self.currency}")
+            f"Your hot wallet balance is {OneInch.parse_float(self.w3.from_wei(self.balance, 'ether'))} {self.currency}"
+        )
 
-    
     def spin(self):
         while True:
             self.check()
             time.sleep(10)
-    
+
     def check(self):
         # check if timestamp is after the deadline
         if time.time() > self.deadline:
             self.execute()
 
     def execute(self):
-        if self.config['vrf']:
+        if self.config["vrf"]:
             self._execute_vrf()
-        elif self.config['blockhash_random']:
+        elif self.config["blockhash_random"]:
             self._execute_blockhash_random()
         else:
             self._execute()
@@ -66,55 +75,50 @@ class Keeper:
         return self.web3.eth.getBalance(self.account.address)
 
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, help='Path to config file')
-    parser.add_argument('--keystore', type=str, help='Path to keystore file')
+    parser.add_argument("--config", type=str, help="Path to config file")
+    parser.add_argument("--keystore", type=str, help="Path to keystore file")
     args = parser.parse_args()
     print(args.config)
     try:
-        with open(args.config, 'r') as f:
+        with open(args.config, "r") as f:
             config = json.load(f)
     except Exception as e:
-        raise Exception('Failed to load config file')
+        raise Exception("Failed to load config file")
     keystore_file = args.keystore
 
     if os.path.isfile(keystore_file):
-            priv_key = Account.decrypt(open_json(keystore_file), getpass.getpass(prompt='Input keystore password: '))
-            self.account = Account.from_key(priv_key)
-            self.has_wallet = True
+        priv_key = Account.decrypt(
+            open_json(keystore_file),
+            getpass.getpass(prompt="Input keystore password: "),
+        )
+        account = Account.from_key(priv_key)
     else:
         print("keystore file not found, creating one")
-        private_key = getpass.getpass(prompt='Input private key: ')
+        private_key = getpass.getpass(prompt="Input private key: ")
         if not private_key.startswith("0x"):
             private_key = "0x" + private_key
         try:
-            self.account = Account.from_key(private_key)
+            account = Account.from_key(private_key)
         except:
             raise Exception("invalid private key...")
-        print(f"private key valid, account: {self.explorer_url}address/{self.account.address}\ncreating keystore file")
+        print(f"private key valid, account: {account.address}\ncreating keystore file")
         for i in range(3):
             password = getpass.getpass("Enter a password for the keystore file: ")
-            password2 = getpass.getpass("Confirm password: ")
+            password_confirmation = getpass.getpass("Confirm password: ")
 
             # Check that the passwords match
-            if password != password2:
+            if password != password_confirmation:
                 print("Passwords do not match")
-                print*(f"Please try again, attempt{i+2}/3")
+                print * (f"Please try again, attempt{i+2}/3")
             else:
                 break
-        if password != password2:
+        if password != password_confirmation:
             raise Exception("Passwords do not match...")
         keystore = Account.encrypt(private_key, password=password)
-        with open(keystore_file, 'w') as f:
+        with open(keystore_file, "w") as f:
             f.write(json.dumps(keystore))
-        self.has_wallet = True
 
-
-
-    #keeper = Keeper(config)
-    #print(keeper.get_balance())
+    keeper = Keeper(config, account)
+    # print(keeper.get_balance())
